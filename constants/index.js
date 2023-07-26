f01dbed164f119cfa649954e6e81fe081aa5892e
miladmatmir70 <106539345+miladmatmir70@users.noreply.github.com>
2023-07-26 13:58:50 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x17e35f72dc7fbb3c848f54ddbf45cc2e883bdbe5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
